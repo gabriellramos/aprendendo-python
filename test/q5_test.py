8226ae0aeb4f33ca95bc89af00c117c469b08196
lucioneves <luciocecilia20@gmail.com>
2022-08-02 15:52:54 -0400

    mudando função

--- test/q5_test.py
+++ test/q5_test.py
@@ -2,7 +2,7 @@ import pytest
 import sys
 import os
 # adding Folder_2 to the system path
-sys.path.append(os.path.join('C:/', 'Users', 'gabri', 'OneDrive', 'Documents', 'GitHub', 'aprendendo-python', 'q5'))
+sys.path.append(os.path.join('C:/', 'Users', 'lucio.neves', 'Desktop', 'python', 'aprendendo-python', 'q5'))
 
 # importing the add and odd_even
 # function
